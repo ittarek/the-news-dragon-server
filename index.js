cbe075ff0ac03e458f95ddc33aef1c02aaef1077
MD Tariqul Islam <ittarek551@gmail.com>
2023-04-29 14:17:27 +0600

    vercel file create

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const catagories = require("./data/catagories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -13,6 +14,27 @@ app.get("/", (req, res) => {
 
 app.get("/catagories", (req, res) => {
   res.send(catagories);
+});
+
+app.get("/catagories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedId = news.find((n) => n._id === id);
+  res.send(selectedId);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
 });
 
 app.listen(port, () => {
